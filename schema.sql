55cbda385fd9f9b4009015790ba6594ca5a29c71
Joe Reidell <jreid2f@gmail.com>
2019-02-22 10:36:30 -0500

    update passenger and station

--- schema.sql
+++ schema.sql
@@ -5,4 +5,30 @@ CREATE TABLE RAIL_LINE (
 
 	CONSTRAINT	Rail_PK
 		PRIMARY KEY(rail_ID)
-)
+);
+
+CREATE TABLE PASSENGER (
+	customer_ID		INTEGER,
+	first_name		VARCHAR(20),
+	last_name		VARCHAR(20),
+	email			VARCHAR(35),
+	phone			CHAR(10),
+	street_address	VARCHAR(50),
+	city			VARCHAR(25),
+	zip				CHAR(5),
+
+	CONSTRAINT Passenger_PK
+		PRIMARY KEY(customer_ID)
+);
+
+CREATE TABLE STATION (
+	station_ID		INTEGER,
+	street_address	VARCHAR(50),
+	city			VARCHAR(25),
+	zip				CHAR(5),
+	open_time		TIME,
+	close_time		TIME,
+
+	CONSTRAINT Station_PK
+		PRIMARY KEY(station_ID)
+);
