9997708c59b5680ffcf72ec42ad9e41b293e8483
Joe Reidell Jr <jreid2f@gmail.com>
2019-04-12 12:00:18 -0400

    add menu

--- src/ExpressRailway.java
+++ src/ExpressRailway.java
@@ -1,12 +1,14 @@
-import java.util.Properties;
+import java.util.*;
 import java.sql.*;
+import com.jakewharton.fliptables.*;
 
 public class ExpressRailway {
 
 	private Connection conn;
 	private Properties props;
+	public static Scanner scan;
 
-	public ExpressRailway() {
+	public ExpressRailway() throws SQLException, ClassNotFoundException {
 		Class.forName("org.postgresql.Driver");
 		String url = "jdbc:postgresql://localhost/zdw9";
 
@@ -14,12 +16,82 @@ public class ExpressRailway {
 		props.setProperty("user","zdw9");
 		props.setProperty("password","example");
 
-		conn = DriverManager.getConnection(url, props); 
+		conn = DriverManager.getConnection(url, props);
 
 		//begin the UI menu
+
 	}
 
-	public static void main(String args[]) throws SQLException, ClassNotFoundException{
+	public static void UIMenu(){
+		scan = new Scanner(System.in);
+		String start = "--------Welcome to Express Railway---------"
+							+ "			Login Menu		"
+							+ "\t1. Log In";
+		
+		String menu = "--------Welcome to Express Railway---------"
+							+ "			Main Menu		"
+							+ "\t1. Single Route Trip Search"
+							+ "\t2. Combination Route Trip Search"
+							+ "\t3. Add Reservation"
+							+ "\t4. Find Specific Train"
+							+ "\t5. Find train with more than one rail line"
+							+ "\t6. Find routes with more than one line"
+							+ "\t7. Find stations that all trains pass through"
+							+ "\t8. Find all trains that does not stop at a specific station"
+							+ "\t9. Find routes that stop at a certain percent of stations"
+							+ "t\10. Display the schedule of a route"
+							+ "t\11. Find the availability of a route at every stop on a specific day"
+							+ "\t12. Import Database"
+							+ "t\13. Export Database"
+							+ "t\14. Delete Database"
+							+ "t\15. Exit";
+		
+		while(true) {
+			System.out.println(menu);
+			System.out.print("Enter your choice: ");
+			String input = scan.next();
+			
+			switch(input) {
+			case "1":
+				break;
+			case "2":
+				break;
+			case "3":
+				break;
+			case "4":
+				break;
+			case "5":
+				break;
+			case "6":
+				break;
+			case "7":
+				break;
+			case "8":
+				break;
+			case "9":
+				break;
+			case "10":
+				break;
+			case "11":
+				break;
+			case "12":
+				break;
+			case "13":
+				break;
+			case "14":
+				break;
+			case "15":
+				System.exit(0);
+				break;
+			default:
+				break;	
+				
+			}
+		}
+		
+	}
+
+	public static void main(String args[]) throws SQLException, ClassNotFoundException {
 		new ExpressRailway();
 	}
 
@@ -30,7 +102,7 @@ public class ExpressRailway {
 		String query1 = "SELECT * FROM TRAIN";
 		ResultSet res1 = st.executeQuery(query1);
 
-		int train_id, top_speed, seats; 
+		int train_id, top_speed, seats;
 		double ppm;
 
 		while (res1.next()) {
